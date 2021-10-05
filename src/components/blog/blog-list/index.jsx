99e60929ff3b4d80ad5a5d72981244cc70098dec
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-10-05 15:57:41 +0100

    node added and fetch

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
-import posts from "../../../data/posts.json";
+// import posts from "../../../data/posts.json";
+
 export default class BlogList extends Component {
+  state = { posts: [] };
+
+  apiUrl = process.env.REACT_APP_BE_URL;
+
+  fetchPosts = async () => {
+    try {
+      let response = await fetch(`${this.apiUrl}/blogPosts`);
+      let postsArray = await response.json();
+      this.setState({ posts: postsArray });
+      return postsArray;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.fetchPosts();
+    // fetchedPosts = fetchPosts();
+    // this.setState({ posts: fetchPosts });
+  }
+
   render() {
     return (
       <Row>
-        {posts.map((post) => (
+        {this.state.posts.map((post) => (
           <Col md={4} style={{ marginBottom: 50 }}>
             <BlogItem key={post.title} {...post} />
           </Col>
